51662fc6e6774a64cbe82e78762fd8926281ac1b
NahianSalsabil <1705091@ugrad.cse.buet.ac.bd>
2022-07-16 03:18:11 +0600

    button added ;-;

--- src/my-components/Content.js
+++ src/my-components/Content.js
@@ -139,11 +139,11 @@ export default function Content() {
   ]
   return (
     <div className='p-2'>
-      <Button/>
-      <Button style={"mybutton-light"}/>
+      {/* <Button/> */}
+      {/* <Button style={"mybutton-light"}/> */}
       {/* <Tenants tenants={tenants}/> */}
       {/* <Owners owners={owners}/> */}
-      {/* <CommitteeMembers committee={committeemembers}/> */}
+      <CommitteeMembers committee={committeemembers}/>
       {/* content */}
     </div>
   )
--- src/my-components/misc/Button.js
+++ src/my-components/misc/Button.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 export default function Button({link, text, style}) {
   return (
-    <a className={`btn mx-2 ${style}`} href={link}>{text}</a>
+    <a className={`btn mx-3 ${style}`} href={link}>{text}</a>
   )
 }
 
