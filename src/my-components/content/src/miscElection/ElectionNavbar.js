2cc3b7a02a4e7dcbddb84567bb5db06df0f0c0c0
Farhana100 <farhanakhanhp@gmail.com>
2022-07-18 10:51:24 +0600

    election page navbar css

--- src/my-components/content/src/miscElection/ElectionNavbar.js
+++ src/my-components/content/src/miscElection/ElectionNavbar.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 export default function ElectionNavbar(props){
     return(
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav class="elec-navbar navbar navbar-expand-lg navbar-light bg-light">
             <a class="nav-item nav-link elec" href="#">Election</a>
             <a class="nav-item nav-link poll" href="#">Poll</a>
         </nav>
