d11eb8170ab0459f84724a4724d6450e6cf8c503
Farhana100 <farhanakhanhp@gmail.com>
2022-08-28 13:44:21 +0600

    image upload test

--- src/my-components/content/src/service/ServiceProviderAdd.js
+++ src/my-components/content/src/service/ServiceProviderAdd.js
@@ -1,245 +1,174 @@
-import React, { useState, useEffect, useRef } from 'react'
-import { Navigate, useNavigate } from 'react-router-dom';
+import React, { useState, useRef } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 import Button from '../../../misc/Button';
 import '../../static/css/serviceProviders.css'
   
 export default function ServiceProviderAdd(){
 
+    let navigate = useNavigate();
     let user = JSON.parse(localStorage.getItem('data'));
 
     if (user.userType !== 'admin') {
-        window.location.replace('/apartments');
+        window.location.replace('/dashboard');
     }
 
     let errorMsg = {
         password:"",
         username:""
     }
+
     
     const [ building, setBuilding ] = useState(user.building);
-    const [ username, setUsername ] = useState("");
-    const [ lastname, setLastname ] = useState("");
-    const [ firstname, setFirstname ] = useState("");
-    const [ password, setPassword ] = useState("");
-    const [ confirmPassword, setConfirmPassword ] = useState("");
-    const [ email, setEmail ] = useState("");
+    const [ company_name, setCompany_name ] = useState("");
+    const [ address, setAddress ] = useState("");
+    const [ image, setImage ] = useState(null);
+    const [ details, setDetails ] = useState("");
+    const [ website, setWebsite ] = useState("");
     const [ phone_number, setPhone_number ] = useState(0);
     const [ bkash_acc_number, setBkash_acc_number ] = useState(0);
-    const [ selectedFiles, setSelectedFiles ] = useState([]);
+    const [ selectedFile, setSelectedFile ] = useState(null);   // temporary
 
-    const handleUsernameChange = (e) => {
-        setUsername(e.target.value);
-        console.log(username);
+    const handleCompany_nameChange = (e) => {
+        setCompany_name(e.target.value);
     }
 
-    const handleFirstnameChange = (e) => {
-        setFirstname(e.target.value);
-        console.log(firstname);
+    const handleAddressChange = (e) => {
+        setAddress(e.target.value);
     }
 
-    const handleLastnameChange = (e) => {
-        setLastname(e.target.value);
-        console.log(lastname);
+    const handleDetailsChange = (e) => {
+        setDetails(e.target.value);
     }
 
-    const handlePasswordChange = (e) => {
-        setPassword(e.target.value);
-        console.log(password);
-    }
-
-    const handleConfirmPasswordChange = (e) => {
-        setConfirmPassword(e.target.value);
-        console.log(e.target.value, password);
-    }
-
-    const handleEmailChange = (e) => {
-        setEmail(e.target.value);
-        console.log(email);
+    const handleWebsiteChange = (e) => {
+        setWebsite(e.target.value);
     }
 
     const handlePhoneNumberChange = (e) => {
         setPhone_number(e.target.value);
-        console.log(phone_number);
     }
 
     const handleBkashNumberChange = (e) => {
         setBkash_acc_number(e.target.value);
-        console.log(bkash_acc_number);
     }
 
 	const handleImageChange = (e) => {
-		// console.log(e.target.files[])
 		if (e.target.files) {
-			const filesArray = Array.from(e.target.files).map((file) => URL.createObjectURL(file));
-
-			// console.log("filesArray: ", filesArray);
-
-			setSelectedFiles((prevImages) => prevImages.concat(filesArray));
-			Array.from(e.target.files).map(
-				(file) => URL.revokeObjectURL(file) // avoid memory leak
-			);
+            setImage(e.target.files[0]);
+			setSelectedFile(URL.createObjectURL(e.target.files[0]));
 		}
 	};
 
 	const renderPhotos = (source) => {
-		console.log('source: ', source);
-		return source.map((photo) => {
-			return <img className='col-4 img-fluid shadow-sm d-block' src={photo} alt="" key={photo} />;
-		});
+        return <img className='col-4 img-fluid shadow-sm d-block' src={source} alt="" key={source} />;
 	};
 
     
-    function createServiceProviderHandler(e){
+    async function createServiceProviderHandler(e){
         e.preventDefault();
-        if(password !== "" && confirmPassword !== password){
-            // console.log("password do not match");
-            errorMsg['password'] = "password do not match";
-        }
-        else {
-            
-            fetch("http://127.0.0.1:8000/createServiceProvider", {
-                method: 'POST',
-                headers: {
-                'Content-type':'application/json',
-                },
-                body: JSON.stringify({
-                    
-                    building: building,
-                    username: username,
-                    lastname: lastname,
-                    firstname: firstname,
-                    password: password,
-                    email: email,
-                    phone_number: phone_number,
-                    bkash_acc_number: bkash_acc_number,
-                    selectedFiles: selectedFiles,
-                })
-            })
-            .then(response => response.json())
-            .then(data => {
-                
-                console.log(data);
-                if(data.success){
-                    navigate('/service');
-                }
-                else {
-                    console.log(data.msg);
-                    errorMsg[data.error] = data.msg;
-                    console.log('new test ', errorMsg['username']);
-                }
-            });
-        }
 
+        let formData = new FormData();
+
+        formData.append('building', building);
+        formData.append('company_name', company_name);
+        formData.append('address', address);
+        formData.append('image', image);
+        formData.append('details', details);
+        formData.append('website', website);
+        formData.append('phone_number', phone_number);
+        formData.append('bkash_acc_number', bkash_acc_number);
+
+        await axios({
+            method: 'post', 
+            url: "http://127.0.0.1:8000/createServiceProvider",
+            data: formData
+        })
+        .then(response => {
+            if(response.data.success){
+                navigate('/service');
+            }
+            else {
+                console.log(response.data.msg);
+                errorMsg[response.data.error] = response.data.msg;
+                console.log('new test ', errorMsg['username']);
+            }
+        });
     }
-
-    let navigate = useNavigate();
-
 
   return (
     <>
         
-        <h4> Add New Service Provider</h4>
-        <hr/>
-        <div className='container-fluid'>
-            <div className='row'>
-                <div className='col-7'>
-                    <form className='container'>
-                        <div className="form-group">
-                            <label className='h6 bold'>Add Service Provider's Image</label>
-                            <div className="row py-4">
-                                <div className="col-lg-6 mx-auto">
-                                    {/* <!-- Upload image input can upload only one image--> */}
-                                    <div className="input-group mb-3 px-2 py-2 rounded-pill bg-white shadow-sm">
-                                        <input type="file" id="file" onChange={handleImageChange}  className="form-control border-0" />
-                                        <label id="file-label" htmlFor="file" className="font-weight-light text-muted">Choose file</label>
-                                        
-                                        <div className="input-group-append">
-                                            <label htmlFor="file" className="btn btn-light m-0 rounded-pill px-4"> <i className="fa fa-cloud-upload mr-2 text-muted"></i><small className="text-uppercase font-weight-bold text-muted">Choose file</small></label>
-                                        </div>
+    <h4> Add New Service Provider</h4>
+    <hr/>
+    <div className='container-fluid'>
+        <div className='row'>
+            <div className='col-7'>
+                <form className='container'>
+                    <div className="form-group">
+                        <label className='h6 bold'>Add Service Provider's Image</label>
+                        <div className="row py-4">
+                            <div className="col-lg-6 mx-auto">
+                                {/* <!-- Upload image input can upload only one image--> */}
+                                <div className="input-group mb-3 px-2 py-2 rounded-pill bg-white shadow-sm">
+                                    <input type="file" id="file" onChange={handleImageChange}  className="form-control border-0" />
+                                    <label id="file-label" htmlFor="file" className="font-weight-light text-muted">Choose file</label>
+                                    
+                                    <div className="input-group-append">
+                                        <label htmlFor="file" className="btn btn-light m-0 rounded-pill px-4"> <i className="fa fa-cloud-upload mr-2 text-muted"></i><small className="text-uppercase font-weight-bold text-muted">Choose file</small></label>
                                     </div>
                                 </div>
                             </div>
-                            {/* <!-- Uploaded image area--> */}
-                            <div className='container-fluid p-0'><div className="result image-area mt-4 row">{renderPhotos(selectedFiles)}</div></div>
-                            
                         </div>
-                        {/* user name */}
-                        <div className="form-group">
-                            <label  className='h6 bold' htmlFor="username">Username: </label>
-                            <input type="text" className="form-control" id="username" aria-describedby="usernameHelp" onChange={handleUsernameChange}/>
-                            <small id="usernameHelp" className="form-text text-muted">{errorMsg['username']}</small>
-                        </div>
-
-                        {/* first name */}
-                        <div className="form-group">
-                            <label  className='h6 bold' htmlFor="firstname">First Name: </label>
-                            <input type="text" className="form-control" id="firstname" aria-describedby="firstnameHelp" onChange={handleFirstnameChange}/>
-                            {/* {errorMsg.firstname !== "" && 
-                                <small id="firstnameHelp" className="form-text text-muted">{errorMsg}</small>
-                            } */}
-                        </div>
-
-                        {/* last name */}
-                        <div className="form-group">
-                            <label  className='h6 bold' htmlFor="lastname">Last Name: </label>
-                            <input type="text" className="form-control" id="lastname" aria-describedby="lastnameHelp" onChange={handleLastnameChange}/>
-                            {/* {errorMsg.lastname !== "" && 
-                                <small id="lastnameHelp" className="form-text text-muted">{errorMsg}</small>
-                            } */}
-                        </div>
-
-                        {/* password */}
-                        <div className="form-group">
-                            <label  className='h6 bold' htmlFor="password">Password: </label>
-                            <input type="password" className="form-control" id="password" aria-describedby="passwordHelp" onChange={handlePasswordChange}/>
-                            {/* <small id="passwordHelp" className="form-text text-muted">this is a test</small> */}
-                            <small id="passwordHelp" className="form-text text-muted">{errorMsg['password']}</small>
-                        </div>
-
-                        {/* confirm password */}
-                        <div className="form-group">
-                            <label  className='h6 bold' htmlFor="confirmPassword">Confirm Password: </label>
-                            <input type="password" className="form-control" id="confirmPassword" aria-describedby="confirmPasswordHelp" onChange={handleConfirmPasswordChange}/>
-                            {/* { errorMsg.password !== "" && 
-                                <small id="confirmPasswordHelp" className="form-text text-muted">{errorMsg.password}</small>
-                            } */}
-                        </div>
-
-                        {/* email */}
-                        <div className="form-group">
-                            <label  className='h6 bold' htmlFor="email">Email Address: </label>
-                            <input type="email" className="form-control" id="email" aria-describedby="emailHelp" onChange={handleEmailChange}/>
-                            {/* {errorMsg.email !== "" && 
-                                <small id="emailHelp" className="form-text text-muted">{errorMsg}</small>
-                            } */}
-                        </div>
-
-                        {/* phone number */}
-                        <div className="form-group">
-                            <label  className='h6 bold' htmlFor="phoneNumber">Phone Number: </label>
-                            <input type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" maxLength={11} minLength={11} className="form-control" id="phoneNumber" aria-describedby="phoneNumberHelp" onChange={handlePhoneNumberChange} required/>
-                            {/* {errorMsg.phone_number !== "" && 
-                                <small id="phoneNumberHelp" className="form-text text-muted">{errorMsg}</small>
-                            } */}
-                        </div>
-
-                        {/* Bkash number */}
-                        <div className="form-group">
-                            <label  className='h6 bold' htmlFor="bkashNumber">Bkash Number: </label>
-                            <input type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" maxLength={11} minLength={11} className="form-control" id="bkashNumber" aria-describedby="bkashNumberHelp" onChange={handleBkashNumberChange} required/>
-                            {/* {errorMsg.bkash_acc_number !== "" && 
-                                <small id="bkashNumberHelp" className="form-text text-muted">{errorMsg.bkash_acc_number}</small>
-                            } */}
-                        </div>
+                        {/* <!-- Uploaded image area--> */}
+                        <div className='container-fluid p-0'><div className="result image-area mt-4 row">{renderPhotos(selectedFile)}</div></div>
                         
-                        <div className='form-group row my-5'>
-                            <div className='col d-flex justify-content-start'><Button text={'Cancel'} link={'/service'} /></div>
-                            <div className='col d-flex justify-content-end'><Button text={'Submit'} OnClick={createServiceProviderHandler}/></div>
-                        </div>
-                    </form>
-                </div>
+                    </div>
+                    {/* company_name */}
+                    <div className="form-group">
+                        <label  className='h6 bold' htmlFor="company_name">Company Name: </label>
+                        <input type="text" className="form-control" id="company_name" aria-describedby="company_nameHelp" onChange={handleCompany_nameChange}/>
+                        <small id="company_nameHelp" className="form-text text-muted">{errorMsg['company_name']}</small>
+                    </div>
+
+                    {/* address */}
+                    <div className="form-group">
+                        <label  className='h6 bold' htmlFor="address">Address: </label>
+                        <input type="text" className="form-control" id="address" aria-describedby="addressHelp" onChange={handleAddressChange}/>
+                    </div>
+
+                    {/* details */}
+                    <div className="form-group">
+                        <label  className='h6 bold' htmlFor="details">Service Details: </label>
+                        <textarea type="textarea" className="form-control" id="details" aria-describedby="detailsHelp" onChange={handleDetailsChange}/>
+                    </div>
+
+                    {/* website */}
+                    <div className="form-group">
+                        <label  className='h6 bold' htmlFor="website">Company Website Link: </label>
+                        <input type="link" className="form-control" id="website" aria-describedby="websiteHelp" onChange={handleWebsiteChange}/>
+                    </div>
+
+                    {/* phone number */}
+                    <div className="form-group">
+                        <label  className='h6 bold' htmlFor="phoneNumber">Phone Number: </label>
+                        <input type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" maxLength={11} minLength={11} className="form-control" id="phoneNumber" aria-describedby="phoneNumberHelp" onChange={handlePhoneNumberChange} required/>
+                    </div>
+
+                    {/* Bkash number */}
+                    <div className="form-group">
+                        <label  className='h6 bold' htmlFor="bkashNumber">Bkash Number: </label>
+                        <input type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" maxLength={11} minLength={11} className="form-control" id="bkashNumber" aria-describedby="bkashNumberHelp" onChange={handleBkashNumberChange} required/>
+                    </div>
+                    
+                    <div className='form-group row my-5'>
+                        <div className='col d-flex justify-content-start'><Button text={'Cancel'} link={'/service'} /></div>
+                        <div className='col d-flex justify-content-end'><Button text={'Submit'} OnClick={createServiceProviderHandler}/></div>
+                    </div>
+                </form>
             </div>
         </div>
+    </div>
     </>
   )
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,37 +1,34 @@
-import React, { Component } from 'react'
-import { Navigate } from "react-router-dom";
+import React, { useState } from 'react'
+import { useNavigate } from "react-router-dom";
 import logo from '../my-components/brand/logo.svg'
 import image1 from '../my-components/content/static/images/login-image.png'
 import '../my-components/content/static/css/login.css'
 import Footer from '../my-components/Footer'
 
 
-export class Login extends Component {
+export default function Login (){
 
-  constructor(props){
-    super(props);
-    this.state = {
-        username: '',
-        password: '',
-        msg: null,
-    }
+  let navigate = useNavigate();
+  let user = JSON.parse(localStorage.getItem('data'));
+  const [ username, setUsername ] = useState('');
+  const [ password, setPassword ] = useState('');
+  const [ msg, setMsg ] = useState('');
+
+  if (user && user.user_active) {
+      window.location.replace('/dashboard');
   }
 
-  handleUsernameChange = (e) => {
-    this.setState({
-        username: e.target.value
-    })
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
   }
 
-  handlePasswordChange = (e) => {
-    this.setState({
-        password: e.target.value
-    })
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
   }
 
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("submit pressed ", this.state);
+    console.log("submit pressed ");
 
     var url = "http://127.0.0.1:8000/login"
     fetch(url, {
@@ -39,72 +36,61 @@ export class Login extends Component {
       headers: {
         'Content-type':'application/json',
       },
-      body: JSON.stringify(this.state)
+      body: JSON.stringify({
+        username: username,
+        password: password
+      })
     })
     .then(response => response.json())
     .then(data => {
-      this.setState({
-        msg : data.msg
-      })
-      console.log(data.msg)
-      console.log(data.token)
-      console.log(data.user_active)
 
-      if (data.user_active){
-        localStorage.setItem('data', JSON.stringify(data))
+      if(data.user_active) {
+        // setMsg(data.msg);
+        localStorage.setItem('data', JSON.stringify(data));
+        navigate('/dashboard');
       }
-      
-      this.state.user_active = data.user_active;
-      window.location.replace('/dashboard');
+      else{
+        setMsg(data.msg);
+      }
     });
   }
 
-  render() {
-    let user = JSON.parse(localStorage.getItem('data'));
-    
-    return (
-      <div>
-        {user && user.user_active && (
-          <Navigate to="/dashboard" replace={true} />
-        )}
+  return (
+    <div>
+      <div className='container-fluid'>
+        <div className='row text-center'>
+          <div className='col-8 d-none d-lg-block p-0 login-col1'>
+            <img className="login-image" src={image1} alt="..." />  
+          </div>
+          <div className='col-lg-4 login-col2'>
+            <form className="form-login">
+              <div className="py-5 login-bg-grad">
+                  <img className="login-logo" src={logo} alt="..." />
+                  <h1 className="fs-3 fw-bolder">ManageCasa</h1>
+              </div>
+              <h1 class="h5 mb-3 font-weight-normal">Please Log in</h1>
 
-        <div className='container-fluid'>
-          <div className='row text-center'>
-            <div className='col-8 d-none d-lg-block p-0 login-col1'>
-              <img className="login-image" src={image1} alt="..." />  
-            </div>
-            <div className='col-lg-4 login-col2'>
-              <form className="form-login">
-                <div className="py-5 login-bg-grad">
-                    <img className="login-logo" src={logo} alt="..." />
-                    <h1 className="fs-3 fw-bolder">ManageCasa</h1>
+              {msg && 
+                <div className="alert alert-danger alert-dismissible fade show" role="alert">
+                  {msg}
+                  <button type="mybutton" className="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
                 </div>
-                <h1 class="h5 mb-3 font-weight-normal">Please Log in</h1>
+              }
 
-                {this.state.msg && 
-                  <div className="alert alert-danger alert-dismissible fade show" role="alert">
-                    {this.state.msg}
-                    <button type="mybutton" className="close" data-dismiss="alert" aria-label="Close">
-                      <span aria-hidden="true">&times;</span>
-                    </button>
-                  </div>
-                }
-
-                <label htmlFor="inputUsername" className="sr-only">Username</label>
-                <input type="username" id="inputUsername" className="form-control" placeholder="Username" value={this.state.username} onChange={this.handleUsernameChange} required autoFocus/>
-                
-                <label htmlFor="inputPassword" className="sr-only">Password</label>
-                <input type="password" id="inputPassword" className="form-control" placeholder="Password" value={this.state.password} onChange={this.handlePasswordChange} required/>
-                
-                <button className="btn btn-lg mybutton btn-block" type="submit" onClick={this.handleSubmit}>Log in</button>
-                <Footer/>
-              </form>
-            </div>
+              <label htmlFor="inputUsername" className="sr-only">Username</label>
+              <input type="username" id="inputUsername" className="form-control" placeholder="Username" value={username} onChange={handleUsernameChange} required autoFocus/>
+              
+              <label htmlFor="inputPassword" className="sr-only">Password</label>
+              <input type="password" id="inputPassword" className="form-control" placeholder="Password" value={password} onChange={handlePasswordChange} required/>
+              
+              <button className="btn btn-lg mybutton btn-block" type="submit" onClick={handleSubmit}>Log in</button>
+              <Footer/>
+            </form>
           </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
-
-export default Login;
